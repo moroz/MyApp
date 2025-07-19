36c3ed41c73c81cda6e4f9f1ba59a3d2a3ba52a5
Karol Moroz <karol@moroz.dev>
2025-07-19 15:00:57 +0800

    Create UUIDv7 SQL function

---
+++ MyApp.Data/Migrations/20250717155142_CreateUUIDv7Function.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUUIDv7Function : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = ReadEmbeddedResource("MyApp.Data.Scripts.CreateUUIDv7Function.sql");
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                drop function if exists uuid7(timestamp with time zone);
+                drop function if exists uuid7();
+                drop extension if exists pgcrypto;
+            ");
+        }
+
+        private static string ReadEmbeddedResource(string name)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using var stream = assembly.GetManifestResourceStream(name);
+            if (stream == null)
+            {
+                throw new Exception("Resource not found");
+            }
+
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+}
